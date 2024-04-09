e37cd3b41c3f78bcf11b4f1b17901da00d5d3262
ali <ali@klay.com>
2024-04-09 14:11:32 +0300

    Auth and workout has mixed and fix some errors

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\challenge;
 use App\Http\Controllers\TestController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\verifyController;
 use App\Http\Controllers\Workout\CategoryController;
 use App\Http\Controllers\Workout\ExerciseController;
 use App\Http\Controllers\Workout\LevelsController;
@@ -22,35 +23,54 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::post('register',[\App\Http\Controllers\UserController::class,'register']);
-
+Route::post('login',[UserController::class,'login']);
+Route::get('logout',[UserController::class,'logout'])->middleware('auth:sanctum');
+Route::post('forgot',[UserController::class,'forgot']);
+Route::post('check_code',[UserController::class,'verfiyReset']);
+Route::post('reset',[UserController::class,'reset']);
+Route::post('verify',[verifyController::class,'verify'])->name('verify')->middleware('auth:sanctum');
 Route::middleware('auth:sanctum')->group(function (){
+    ///////
     Route::get('/user',[UserController::class,'getUser']);
+    ///////
     Route::get('/allchallenges',[challenge::class,'returnAll']);
     Route::get('getchallenge/{name}',[\App\Http\Controllers\challenge::class,'Getchallenge']);
+    //////
     Route::get('getChallInfo/{challenge_id}',[challenge::class,'getChallInfo']);
+    ////////
     Route::get('/enroll/{challenge_id}',[challenge::class,'enroll']);
+    ////////
     Route::put('/completed/{challenge_id}',[challenge::class,'endOfChallenge']);
+    ///////
     Route::post('challenge',[\App\Http\Controllers\challenge::class,'addchallenge'])->middleware('admin');
-    Route::post('add',[\App\Http\Controllers\Admin::class,'Add']);
+    //Route::post('add',[\App\Http\Controllers\Admin::class,'Add']);
     Route::post('advice',[\App\Http\Controllers\Coach::class,'advice']);
     Route::get('Get/{id}',[\App\Http\Controllers\Coach::class,'getadvice']);
     Route::post('Favorite',[UserController::class,'Favorite']);
     Route::get('GetFavorite/{id}',[UserController::class,'GetFavorite']);
+    ////////
     Route::post('/is_done',[TestController::class,'verfiyCategory']);
+    ///////
     Route::get('/record',[TestController::class,'getRecord']);
+    //////
     Route::post('add',[\App\Http\Controllers\UserController::class,'Add']);
+    ///////
     Route::resource('exercise',ExerciseController::class);
     Route::post('calculate',[\App\Http\Controllers\ProgressController::class,'calculate']);
     Route::post('TargetWeight',[\App\Http\Controllers\ProgressController::class,'TargetWeight']);
+    ////
+    Route::get('search/{id}',[ExerciseController::class,'Search']);
+    ////
+    Route::resource('level', LevelsController::class);
+    ////
+    Route::resource('categaroy',CategoryController::class);
 });
 Route::get('getexe',[ExerciseController::class,'Getexe']);
 Route::post('image',[UserController::class,'image']);
 Route::get('addFavorite/{id}',[UserController::class,'Favorite']);
 Route::get('good/{id}',[\App\Http\Controllers\Coach::class,'good']);
 Route::get('getCoach',[\App\Http\Controllers\Coach::class,'GetCoach']);
-Route::get('search/{id}',[ExerciseController::class,'Search']);
-Route::resource('level', LevelsController::class);
-Route::resource('categaroy',CategoryController::class);
+
 
 Route::post('advice',[\App\Http\Controllers\coach::class,'advice']);
 
