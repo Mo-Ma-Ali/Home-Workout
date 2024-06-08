55b127ba3deb6a5177078b8bec65c523c39e5a60
modar <modar.bayazed25@gmail.com>
2024-06-08 16:07:23 +0300

    new

--- routes/api.php
+++ routes/api.php
@@ -27,6 +27,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::post('Payment',[\App\Http\Controllers\Payment::class,'makePayment']);
+
+
 Route::get('GetCategory',[\App\Http\Controllers\ProductController::class,'GetCategoryProduct']);
 Route::get('GetProduct/{id}',[\App\Http\Controllers\ProductController::class,'GetProductFromId']);
 Route::post('register',[\App\Http\Controllers\UserController::class,'register']);
