309c38a730cc7f42fb4f90c5d20bf5ba436b2305
ali <ali@klay.com>
2024-07-19 13:09:02 +0300

    update the advice

--- routes/api.php
+++ routes/api.php
@@ -36,62 +36,6 @@ Route::get('Tra',[\App\Http\Controllers\Translate::class,'Translate']);
 Route::get('TraE',[\App\Http\Controllers\Translate::class,'TranslateExercise']);
 Route::post('Payment',[\App\Http\Controllers\Payment::class,'makePayment']);
 
-Route::get('/testnotification', function () {
-
-    $fcm = "dwkcDf0hSYOdSWj_OiCaol:APA91bFSrhbeUf5DcMLDePwoNe6y0VtjwU_gumWLN2uq4h1wISXp30j0XT1zNIIkpbv8NxZ9APRE9wirQlT7vk2ruoS_7NWOI08BTm0Y_M858zfBXwZu01ZURwE-i6CN3SWMzeY-r-7K";
-
-    $title = "اشعار جديد";
-    $description = "تيست تيست تيست";
-
-    $credentialsFilePath = "/json/file.json";  // local
-    $credentialsFilePath = Http::get(asset('json/file.json')); // in server
-    $client = new GoogleClient();
-    $client->setAuthConfig($credentialsFilePath);
-    $client->addScope('https://www.googleapis.com/auth/firebase.messaging');
-    $client->refreshTokenWithAssertion();
-    $token = $client->getAccessToken();
-
-    $access_token = $token['access_token'];
-
-    $headers = [
-        "Authorization: Bearer $access_token",
-        'Content-Type: application/json'
-    ];
-
-    $data = [
-        "message" => [
-            "token" => $fcm,
-            "notification" => [
-                "title" => $title,
-                "body" => $description,
-            ],
-        ]
-    ];
-    $payload = json_encode($data);
-
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, 'https://fcm.googleapis.com/v1/projects/training-app-12f6c/messages:send');
-    curl_setopt($ch, CURLOPT_POST, true);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
-    curl_setopt($ch, CURLOPT_VERBOSE, true); // Enable verbose output for debugging
-    $response = curl_exec($ch);
-    $err = curl_error($ch);
-    curl_close($ch);
-
-    if ($err) {
-        return response()->json([
-            'message' => 'Curl Error: ' . $err
-        ], 500);
-    } else {
-        return response()->json([
-            'message' => 'Notification has been sent',
-            'response' => json_decode($response, true)
-        ]);
-    }
-})->name('testnotification');
 
 Route::get('GetCategory',[\App\Http\Controllers\ProductController::class,'GetCategoryProduct']);
 Route::get('GetProduct/{id}',[\App\Http\Controllers\ProductController::class,'GetProductFromId']);
